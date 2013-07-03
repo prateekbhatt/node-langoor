e8f0e17393cd0565c51ef950ec976b474c6f8f8c
prateekbhatt <prattbhatt@gmail.com>
2013-07-03 12:17:53 +0530

    added views layout and index

--- routes/subscription.js
+++ routes/subscription.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.index = function (req, res) {
+  req.flash('success', 'Welcome to langoor!')
   res.render('index',
     { success: req.flash('success'), error: req.flash('error') });
 };
