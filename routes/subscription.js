0d4a89a5ecb7125fd41b18869b7b2b5e6ba4028c
prateekbhatt <prattbhatt@gmail.com>
2013-07-03 13:00:53 +0530

    modified subscription routes and views

--- routes/subscription.js
+++ routes/subscription.js
@@ -1,7 +1,25 @@
 'use strict';
 
-exports.index = function (req, res) {
-  req.flash('success', 'Welcome to langoor!')
-  res.render('index',
-    { success: req.flash('success'), error: req.flash('error') });
+var path = require('path')
+  , rootDir = path.join(__dirname, '..')
+  , Subscription = require(path.join(rootDir, 'models', 'Subscription'))
+  ;
+
+exports.index = function (req, res){
+  Subscription.find({}, function (err, subs){
+    if (err) return next(err);
+    res.subscribers = subs;
+    res.render('index',
+      { success: req.flash('success'), error: req.flash('error') });
+  });
+};
+
+exports.create = function (req, res){
+  var newSub = req.body.sub;
+
+  Subscription.create(newSub, function (err, savedSub){
+    if (err) return next(err);
+    req.flash('success', 'Added new subscription');
+    res.redirect('/');
+  });
 };
